487dd201fc5ea5cbe50466c621e8599671fcaac7
Nicole Jocson <nicpjocson@gmail.com>
2024-03-27 02:43:27 +0800

    Add project files.

---
+++ asm_main.asm
@@ -0,0 +1,7 @@
+%include "io64.inc"
+section .text
+global main
+main:
+    ;write your code here
+    xor rax, rax
+    ret
---
+++ main.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <windows.h>
+
+extern void asm_main();
+
+int main() {
+	asm_main();
+	return 0;
+}
