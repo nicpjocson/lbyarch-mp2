c12e4519d32a66b828b20290870c1d68963e70ea
wonchuz <120187423+wonchuz@users.noreply.github.com>
2024-03-30 23:50:22 +0800

    Refactor C version

--- main.c
+++ main.c
@@ -3,39 +3,6 @@
 #include <windows.h>
 
 extern int asm_main(/*int a1, int a2, int a3, int a4, int a5, int a6, int a7*/);
-void stencil_kernel(float* X, float* Y, int n);
-void printOutput(float* Y, int n);
-
-int main(int argc, char* argv[]) {
-	// Input: Scalar variable n (integer) contains the length of the vector
-	// Vectors X and Y are both single-precision float.
-    int n;
-    float ans;
-    float* X, * Y;
-
-    printf("Input vector length: ");
-    scanf("%d", &n);
-
-    X = (float*)malloc(n * sizeof(float));
-    Y = (float*)malloc(n * sizeof(float));
-
-    if (X == NULL || Y == NULL) {
-        printf("Memory allocation failed\n");
-        return 1;
-    }
-
-    for (int i = 0; i < n; i++) {
-        X[i] = i + 1;
-    }
-
-    // Y[i] = X[i-3] + X[i-2] + X[i-1] + X[i] + X[i+1] + X[i+2] + X[i+3]
-    ans = asm_main();
-    stencil_kernel(X, Y, n);
-    // Output: store result in vector Y.
-    // Display the result of 1st ten elements of vector Y for all versions of kernel (i.e., C and x86-64).
-    printOutput(Y, n); // C
-    return 0;
-}
 
 void stencil_kernel(float* X, float* Y, int n) {
     for (int i = 3; i < n - 3; i++) {
@@ -58,4 +25,49 @@ void printOutput(float* Y, int n) {
             printf("%.2lf ", Y[i]);
         }
     }
+}
+
+float* allocateMemory(int n) {
+    float* arr = (float*)malloc(n * sizeof(float));
+    if (arr == NULL) {
+        printf("Memory allocation failed\n");
+        exit(1);
+    }
+    return arr;
+}
+
+int main(int argc, char* argv[]) {
+    int n; // Vector Length
+    float* X, * Y;
+
+    printf("Input vector length: ");
+    scanf("%d", &n);
+
+    X = allocateMemory(n);
+    Y = allocateMemory(n);
+
+    // Initialize
+    for (int i = 0; i < n; i++) {
+        X[i] = i + 1;
+    }
+
+    // C version
+    stencil_kernel(X, Y, n);
+
+    // Output: store result in vector Y.
+    // Display the result of 1st ten elements of vector Y for all versions of kernel (i.e., C and x86-64).
+    printf("C version Output: ");
+    printOutput(Y, n);
+
+    // Assembly Version
+    free(Y);
+    Y = allocateMemory(n);
+;   // ans = asm_main();
+    printf("x86 - 64 Version Output: ");
+    printOutput(Y, n);
+
+    free(X);
+    free(Y);
+
+    return 0;
 }
