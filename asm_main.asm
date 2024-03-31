1f32d50cf9993f94d0788b3d0795f3895e093edd
wonchuz <120187423+wonchuz@users.noreply.github.com>
2024-04-01 04:24:24 +0800

    Update asm_main.asm

--- asm_main.asm
+++ asm_main.asm
@@ -9,34 +9,28 @@ asm_main:
     push rbp
     mov rbp, rsp
 
-    mov rsi, [rbp + 8]  ; X array
-    mov rdi, [rbp + 12] ; Y array
-    mov ecx, [rbp + 16] ; size
-    sub ecx, 6
+    mov rdi, [rbp+8]
+    mov rsi, [rbp+16] 
+    ;mov r8d, [rbp+24]
 
-    add rsi, 12
-    mov ebx, 0
+    ;sub r8d, 6
+    add rdi, 12
+    mov r8d, 2
 stencil_kernel:    
-    movss xmm0, [rsi + ebx - 12]
-    movss xmm1, [rsi + ebx - 8]
-    movss xmm2, [rsi + ebx - 4]
-    movss xmm3, [rsi + ebx]
-    movss xmm4, [rsi + ebx + 4]
-    movss xmm5, [rsi + ebx + 8]
-    movss xmm6, [rsi + ebx + 12]
+    movss xmm0, [rdi - 12]
+    movss xmm1, [rdi - 8]
+    movss xmm2, [rdi - 4]
+    movss xmm3, [rdi]
+    movss xmm4, [rdi + 4]
+    movss xmm5, [rdi + 8]
     
-    addss xmm6, xmm0
-    addss xmm6, xmm1
-    addss xmm6, xmm2
-    addss xmm6, xmm3
-    addss xmm6, xmm4
-    addss xmm6, xmm5
 
-    movss [rdi + ebx], xmm6
-    add ebx, 4
-    dec ecx
+    movss [rsi], xmm3
+    dec r8d
+    add rdi, 4
+    add rsi, 4
 
-    cmp ecx, 0
+    cmp r8d, 0
     jnz stencil_kernel
 
     pop rbp
