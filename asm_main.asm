e0447277ab6cbb62e1b951a95d7e8f6695b2c3b4
wonchuz <120187423+wonchuz@users.noreply.github.com>
2024-04-01 01:46:38 +0800

    Update asm_main.asm

--- asm_main.asm
+++ asm_main.asm
@@ -6,37 +6,38 @@ extern printf
 global asm_main
 
 asm_main:
-    lea rdi, [rsp+8]     ; Load pointer to X into rdi
-    lea rsi, [rsp+12]    ; Load pointer to Y into rsi
-    mov rdx, [rsp+16]    ; Load n into rdx
-    
-    sub rdx, 6 
-    add rdi, 12
-stencil_kernel:
-    cmp rdx, 6            
-    jz fin                ; Exit loop if rdx = 0
-    movss xmm0, [rdi - 12]; Load value X[i-3] into xmm0
-    movss xmm1, [rdi - 8] ; Load value X[i-2] into xmm1
-    movss xmm2, [rdi - 4] ; Load value X[i-1] into xmm2
-    movss xmm3, [rdi]     ; Load value X[i] into xmm3
-    movss xmm4, [rdi + 4] ; Load value X[i+1] into xmm4
-    movss xmm5, [rdi + 8] ; Load value X[i+2] into xmm5
-    movss xmm6, [rdi + 12]; Load value X[i+3] into xmm6
-    
-    addss xmm7, xmm0
-    addss xmm7, xmm1
-    addss xmm7, xmm2
-    addss xmm7, xmm3
-    addss xmm7, xmm4
-    addss xmm7, xmm5
-    addss xmm7, xmm6
+    push rbp
+    mov rbp, rsp
 
-    movss [rbx], xmm7
-    subss xmm7, xmm7
+    mov rsi, [rbp + 8]  ; X array
+    mov rdi, [rbp + 12] ; Y array
+    mov ecx, [rbp + 16] ; size
+    sub ecx, 6
 
-    add rdi, 4
-    add rsi, 4
-    dec rdx
-    jmp stencil_kernel
-fin:
+    add rsi, 12
+    mov ebx, 0
+stencil_kernel:    
+    movss xmm0, [rsi + ebx - 12]
+    movss xmm1, [rsi + ebx - 8]
+    movss xmm2, [rsi + ebx - 4]
+    movss xmm3, [rsi + ebx]
+    movss xmm4, [rsi + ebx + 4]
+    movss xmm5, [rsi + ebx + 8]
+    movss xmm6, [rsi + ebx + 12]
+    
+    addss xmm6, xmm0
+    addss xmm6, xmm1
+    addss xmm6, xmm2
+    addss xmm6, xmm3
+    addss xmm6, xmm4
+    addss xmm6, xmm5
+
+    movss [rdi + ebx], xmm6
+    add ebx, 4
+    dec ecx
+
+    cmp ecx, 0
+    jnz stencil_kernel
+
+    pop rbp
     ret
