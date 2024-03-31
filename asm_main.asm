3a8d42aa53697ee92558d9a12445ae021f2f0fbd
wonchuz <120187423+wonchuz@users.noreply.github.com>
2024-03-31 22:37:23 +0800

    changed registers and changed segement to section

--- asm_main.asm
+++ asm_main.asm
@@ -1,33 +1,28 @@
 section .data
-ya dd 0.0
-segment .text
-    bits 64
-    ;default rel
-    ;extern printf
-    global asm_main
+section .text
+bits 64
+default rel
+extern printf
+global asm_main
 
 asm_main:
-    lea rax, [rsp+8]     ; Load pointer to X into eax
-    lea rbx, [rsp+12]    ; Load pointer to Y into ebx
-    mov rcx, [rsp+16]    ; Load n into ecx
+    lea rdi, [rsp+8]     ; Load pointer to X into rdi
+    lea rsi, [rsp+12]    ; Load pointer to Y into rsi
+    mov rdx, [rsp+16]    ; Load n into rdx
     
-    sub rcx, 6 
-    add rax, 12
-    
-    movss xmm7, [ya]
-    ; start loop in the 4th element
+    sub rdx, 6 
+    add rdi, 12
 stencil_kernel:
-    cmp ecx, 0
-    jz fin
-    movss xmm0, [rax - 12] ; Gte values [x-3]
-    movss xmm1, [rax - 8] ; [x-2]
-    movss xmm2, [rax - 4] ; [x-1]
-    movss xmm3, [rax] ; [x]
-    movss xmm4, [rax + 4] ; [x+1]
-    movss xmm5, [rax + 8] ; [x+2]
-    movss xmm6, [rax + 12] ; [x+3]
+    cmp rdx, 6            
+    jz fin                ; Exit loop if rdx = 0
+    movss xmm0, [rdi - 12]; Load value X[i-3] into xmm0
+    movss xmm1, [rdi - 8] ; Load value X[i-2] into xmm1
+    movss xmm2, [rdi - 4] ; Load value X[i-1] into xmm2
+    movss xmm3, [rdi]     ; Load value X[i] into xmm3
+    movss xmm4, [rdi + 4] ; Load value X[i+1] into xmm4
+    movss xmm5, [rdi + 8] ; Load value X[i+2] into xmm5
+    movss xmm6, [rdi + 12]; Load value X[i+3] into xmm6
     
-    ; Do the thing
     addss xmm7, xmm0
     addss xmm7, xmm1
     addss xmm7, xmm2
@@ -35,12 +30,13 @@ stencil_kernel:
     addss xmm7, xmm4
     addss xmm7, xmm5
     addss xmm7, xmm6
-    movss [ebx], xmm7
-    
+
+    movss [rbx], xmm7
     subss xmm7, xmm7
-    add rax, 4
-    add rbx, 4
-    dec rcx
+
+    add rdi, 4
+    add rsi, 4
+    dec rdx
     jmp stencil_kernel
-fin: NOP
+fin:
     ret
